18d69d9005f1e3b5199285ef4996634558607d3e
Denielkas <132719249+Denielkas@users.noreply.github.com>
2023-11-28 11:18:28 -0300

    login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,26 +1,46 @@
-function Login () {
+import React, { useState } from 'react';
 
-    return (
-        <form className="flex flex-col items-start">
-            <h1 className="font-primary text-white text-[20px] p-4 mb-0 -ml-4">Digite seu CPF</h1>
+    const Login = () => {
+      const [cpf, setCpf] = useState('');
+      const [isChecked, setIsChecked] = useState(false);
+
+      const handleCpfChange = (e) => {
+        setCpf(e.target.value);
+      };
+
+      const handleCheckboxChange = () => {
+        setIsChecked(!isChecked);
+      };
+
+      const handleLogin = () => {
+        console.log('CPF:', cpf);
+        console.log('Checkbox está', isChecked ? 'selecionado' : 'desselecionado');
+      };
+
+  return (
+      <form className="flex flex-col items-start">
+          <h1 className="font-primary text-white text-[20px] p-4 mb-0 -ml-4">Digite seu CPF</h1>
+          <input
+          type="text"
+          className="w-full h-10 bg-opacity-100 -mb-2 bg-input border-2 border-greenBorder p-4 rounded-md outline-none font-primary text-sm text-white"
+          id="cpf"
+          value={cpf}
+          onChange={handleCpfChange}
+          placeholder="Digite seu CPF"
+          />
+          <label>
             <input
-            type="text"
-            className="w-full h-10 bg-opacity-100 -mb-2 bg-input border-2 border-greenBorder p-4 rounded-md outline-none font-primary text-sm text-white"
-            placeholder="Digite aqui"
+              type="checkbox"
+              className="text-white text-[10px] p-4 mb-0 -ml-4"
+              checked={isChecked}
+              onChange={handleCheckboxChange}
             />
-            <div className="flex items-center mt-4">
-                <input
-                type="checkbox"
-                id="keepLoggedIn"
-                className="mr-2"
-                />
-                <label htmlFor="keepLoggedIn" className="text-white text-[10px]">
-                Manter-me conectado
-                </label>
-            </div>
-            <button type="submit" className="h-10 bg-greenBg w-full mt-5 border-greenBorder p-2 rounded-md outline-none font-primary text-sm text-white">Logar</button>
-        </form>
-    )
+            Manter-me Conectado
+          </label>
+
+          <button type="submit" onClick={handleLogin} className="h-10 bg-greenBg w-full mt-5 border-greenBorder p-2 rounded-md outline-none font-primary text-sm text-white">Logar</button>
+      </form>
+  )
 
 }
 
