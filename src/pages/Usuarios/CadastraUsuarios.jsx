d7d325dce1e730e8a239ac2e4f2c53f2e3bab41e
Lucas Freitas <lucasdefreitas813@gmail.com>
2023-12-04 02:57:48 -0300

    Criação da página de cadastro de usuários, sem nada funcional

--- src/pages/Usuarios/CadastraUsuarios.jsx
+++ src/pages/Usuarios/CadastraUsuarios.jsx
@@ -1,9 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-function CadastraUsuarios () {
+function CadastraUsuarios() {
     return (
-        <div></div>
-    )
+        <div>
+        <table className="w-[80%] border-green-500 border-2 mx-auto my-auto mt-24">
+            <thead className="bg-green-500 text-white">
+            <tr>
+                <th className="w-44 border-green-500 border-b-2 p-2 text-center">Nome Completo</th>
+                <th className="w-32 border-green-500 border-b-2 p-2 text-center">CPF</th>
+                <th className="w-32 border-green-500 border-b-2 p-2 text-center">Admin</th>
+            </tr>
+            </thead>
+
+            <tbody>
+          
+                <tr >
+                    <td className="w-44 border-green-500 border-b-2 p-2 text-center"></td>
+                    <td className="w-32 border-green-500 border-b-2 p-2 text-center"></td>
+                    <td className="w-32 border-green-500 border-b-2 p-2 text-center"></td>
+                </tr>
+            
+            </tbody>
+        </table>
+        </div>
+    );
 }
 
 export default CadastraUsuarios;
